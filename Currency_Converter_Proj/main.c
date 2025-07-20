e29c9f9233edaa4e7d71aabc15cc5bafa0a4a492
Gleb Danylov <118921940+Zzorro-Kid@users.noreply.github.com>
2025-07-20 13:44:40 +0200

    Update main.c

--- Currency_Converter_Proj/main.c
+++ Currency_Converter_Proj/main.c
@@ -1,107 +1,52 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    char conv_names[][50] = {
+    
+    string conv_names[] = {
+    
         "USD/RUB", "USD/KZT", "USD/UZS", "EUR/USD", "USD/UAH",
-        "EUR/RUB", "RUB/KZT", "USD/CNY", "USD/BYN", "RUB/UZS",
+        "EUR/RUB", "RUB/KZT", "USD/CNY", "USD/BYN", "RUB/UZS"
     };
 
     float conv_rates[] = {
-        78.4, 518.5, 12468, 1.1637, 41.7, 
-        91, 6.6, 7.17, 2.60, 159
+    
+        78.4f, 518.5f, 12468.0f, 1.1637f, 41.7f,
+        91.0f, 6.6f, 7.17f, 2.60f, 159.0f
     };
 
     int selected_conv_num;
     float input_value;
     float converted_value;
 
-    printf("List:\n \n0: USD/RUB\n \n1: USD/KZT\n \n2: USD/UZS\n \n3: EUR/USD\n \n4: USD/UAH\n \n5: EUR/RUB\n \n6: RUB/KZT\n \n7: USD/CNY\n \n8: USD/BYN\n \n9: RUB/UZS\n ");
-
-    printf("\nChoose your option (0-9): ");
-    scanf("%d", &selected_conv_num);
-
-    printf("Write the value to convert: ");
-    scanf("%f", &input_value);
-
-    switch(selected_conv_num) {
-        case 0:
-            converted_value = input_value * conv_rates[0];
-        break;
-
-        case 1:
-            converted_value = input_value * conv_rates[1];
-        break;
-
-        case 2:
-            converted_value = input_value * conv_rates[2];
-        break;
-
-        case 3:
-            converted_value = input_value * conv_rates[3];
-        break;
-
-        case 4:
-            converted_value = input_value * conv_rates[4];
-        break;
-
-        case 5:
-            converted_value = input_value * conv_rates[5];
-        break;
-
-        case 6:
-            converted_value = input_value * conv_rates[6];
-        break;
-
-        case 7:
-            converted_value = input_value * conv_rates[7];
-        break;
-
-        case 8:
-            converted_value = input_value * conv_rates[8];
-        break;
-
-        case 9:
-            converted_value = input_value * conv_rates[9];
-        break;
-
-        default:
-            converted_value = -1.0;
-            printf("Error");
-        break;
+    cout << "List:\n\n";
+    for (int i = 0; i < 10; ++i) {
+        
+        cout << i << ": " << conv_names[i] << "\n\n";
     }
 
-    if (selected_conv_num == 0) {
-        printf("%.2fUSD = %.2fRUB", input_value, converted_value);
+    cout << "Choose your option (0-9): ";
+    cin >> selected_conv_num;
 
-    } else if (selected_conv_num == 1) {
-        printf("%.2fUSD = %.2fKZT", input_value, converted_value);
+    cout << "Write the value to convert: ";
+    cin >> input_value;
 
-    } else if (selected_conv_num == 2) {
-        printf("%.2fUSD = %.2fUZS", input_value, converted_value);
+    if (selected_conv_num >= 0 && selected_conv_num < 10) {
         
-    } else if (selected_conv_num == 3) {
-        printf("%.2fEUR = %.2fUSD", input_value, converted_value);
-        
-    } else if (selected_conv_num == 4) {
-        printf("%.2fUSD = %.2fUAH", input_value, converted_value);
-        
-    } else if (selected_conv_num == 5) {
-        printf("%.2fEUR = %.2fRUB", input_value, converted_value);
-        
-    } else if (selected_conv_num == 6) {
-        printf("%.2fRUB = %.2fKZT", input_value, converted_value);
-        
-    } else if (selected_conv_num == 7) {
-        printf("%.2fUSD = %.2fCNY", input_value, converted_value);
-        
-    } else if (selected_conv_num == 8) {
-        printf("%.2fUSD = %.2fBYN", input_value, converted_value);
-        
-    } else if (selected_conv_num == 9) {
-        printf("%.2fRUB = %.2fUZS", input_value, converted_value);
-        
+        converted_value = input_value * conv_rates[selected_conv_num];
+
+        cout.precision(2);
+        cout << fixed;
+
+        string from = conv_names[selected_conv_num].substr(0, 3);
+        string to   = conv_names[selected_conv_num].substr(4, 3);
+
+        cout << input_value << from << " = " << converted_value << to << endl;
     } else {
-        printf("Error");
+        
+        cout << "Error: Invalid option." << endl;
     }
 
     return 0;
